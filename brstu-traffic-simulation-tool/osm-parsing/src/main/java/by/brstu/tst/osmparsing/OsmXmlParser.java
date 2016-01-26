40f3c0ae56c7181cc2fd65356829bb8b3c600717
Andrei Klimovich <kwetril@gmail.com>
2016-01-26 23:29:26 +0300

    add geotools for coordinate projection, try to transform OsmNode coordinates

--- brstu-traffic-simulation-tool/osm-parsing/src/main/java/by/brstu/tst/osmparsing/OsmXmlParser.java
+++ brstu-traffic-simulation-tool/osm-parsing/src/main/java/by/brstu/tst/osmparsing/OsmXmlParser.java
@@ -2,12 +2,22 @@ package by.brstu.tst.osmparsing;
 
 import de.topobyte.osm4j.core.access.OsmIterator;
 import de.topobyte.osm4j.core.model.iface.*;
+import org.geotools.geometry.DirectPosition2D;
+import org.geotools.geometry.GeneralDirectPosition;
+import org.geotools.referencing.CRS;
+import org.opengis.geometry.Geometry;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.MathTransform;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by kwetril on 1/26/16.
  */
 public class OsmXmlParser {
     public void parse(OsmIterator iterator) {
+        List<OsmNode> nodes = new ArrayList<>();
         for (EntityContainer container : iterator) {
             switch (container.getType()) {
                 case Node:
@@ -15,6 +25,7 @@ public class OsmXmlParser {
                     OsmNode node = (OsmNode) container.getEntity();
                     System.out.format("Node info: id=%s, lon=%s, lat=%s\n",
                             node.getId(), node.getLongitude(), node.getLatitude());
+                    nodes.add(node);
                     break;
                 case Relation:
                     System.out.println("Relation found");
@@ -29,6 +40,23 @@ public class OsmXmlParser {
                             way.getId(), way.getNumberOfNodes());
                     break;
             }
+        }
+
+        OsmNode firstNode = nodes.get(0);
+        try {
+            CoordinateReferenceSystem source = CRS.decode("EPSG:4326");
+            CoordinateReferenceSystem target = CRS.decode("EPSG:3857");
+            MathTransform transform = CRS.findMathTransform(source, target, false);
+            DirectPosition2D position = new DirectPosition2D(firstNode.getLatitude(), firstNode.getLongitude());
+            GeneralDirectPosition transformedPosition = (GeneralDirectPosition) transform.transform(position, null);
+            System.out.printf("%s, %s\n", transformedPosition.getOrdinate(0), transformedPosition.getOrdinate(1));
+
+            double test[] = new double[] {firstNode.getLatitude(), firstNode.getLongitude()};
+            double res[] = new double[2];
+            transform.transform(test, 0, res, 0, 1);
+            System.out.printf("%s, %s\n", res[0], res[1]);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
